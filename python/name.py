d12c4447e142ab6ac7791bca1d8529961dcec7e6
rangamuni <grangamunii@gmail.com>
2026-03-06 15:09:21 +0530

    changes

---
+++ python/name.py
@@ -0,0 +1,6 @@
+name=input("Enter name: ")
+city=input("Enter city: ")
+institute=input("Enter institute name:")
+course=input("Enter course name: ")
+op=f"i am {name}, came to {city} and joined in {institute} for {course} course"
+print(op)
