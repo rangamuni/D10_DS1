72b83eea929c2f01376c72602ec9eb699f1593b8
rangamuni <grangamunii@gmail.com>
2026-03-06 15:32:31 +0530

    type,print,id transformationms

--- python/name.py
+++ python/name.py
@@ -1,6 +1,12 @@
-name=input("Enter name: ")
-city=input("Enter city: ")
-institute=input("Enter institute name:")
-course=input("Enter course name: ")
-op=f"i am {name}, came to {city} and joined in {institute} for {course} course"
-print(op)
+# name=input("Enter name: ")
+# city=input("Enter city: ")
+# institute=input("Enter institute name:")
+# course=input("Enter course name: ")
+# op=f"i am {name}, came to {city} and joined in {institute} for {course} course"
+# print(op)
+x=print("nhytfbkh")
+y='123'
+z=id(y)
+print(x)
+print(z)
+print(type(y))
