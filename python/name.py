0512bdca6e9d81bb1f3d56d42ac47d647b2c8e11
rangamuni <grangamunii@gmail.com>
2026-03-06 15:53:09 +0530

    change

--- python/name.py
+++ python/name.py
@@ -4,9 +4,14 @@
 # course=input("Enter course name: ")
 # op=f"i am {name}, came to {city} and joined in {institute} for {course} course"
 # print(op)
-x=print("nhytfbkh")
-y='123'
-z=id(y)
-print(x)
-print(z)
-print(type(y))
+# x=print("nhy tfb kh")
+# y='123'
+# z=id(y)
+# print(x)
+# print(z)
+# print(type(y))
+# print(bool([1]))
+
+num=12345
+num1=str(num)
+print(num1[4])
